f910b4aa277ffa274422e7122e25e21e0646cdf5
Advik <advik.b@gmail.com>
2025-07-30 16:30:17 +0530

    The protobuf output

---
+++ frontend/src/lib/proto/nodetype.js
@@ -0,0 +1,31 @@
+// source: axon.proto
+/**
+ * @fileoverview
+ * @enhanceable
+ * @suppress {missingRequire} reports error on implicit type usages.
+ * @suppress {messageConventions} JS Compiler reports an error if a variable or
+ *     field starts with 'MSG_' and isn't a translatable message.
+ * @public
+ */
+// GENERATED CODE -- DO NOT EDIT!
+/* eslint-disable */
+// @ts-nocheck
+
+goog.provide('proto.axon.NodeType');
+
+/**
+ * @enum {number}
+ */
+proto.axon.NodeType = {
+  NODE_UNKNOWN: 0,
+  START: 1,
+  END: 2,
+  RETURN: 3,
+  FUNC_DEF: 4,
+  STRUCT_DEF: 5,
+  CONSTANT: 6,
+  FUNCTION: 7,
+  OPERATOR: 8,
+  IGNORE: 9
+};
+
